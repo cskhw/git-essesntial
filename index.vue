a7b1b45d03b4841573df1b726749fce09d5f6aee
cskhw <51100329+cskhw@users.noreply.github.com>
2022-12-12 15:58:05 +0900

    feat: 싱크 맞춤

--- index.vue
+++ index.vue
@@ -1,12 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div>rebase 테스트 1</div>
-  <div>rebase 테스트 2</div>
-=======
   <div>dev 브랜치 rebase 1</div>
-<<<<<<< HEAD
->>>>>>> d2f1f29 (feat: dev rebase 1)
-=======
   <div>dev 브랜치 rebase 2</div>
->>>>>>> bb1930e (feawt: dev rebase 2)
 </template>
